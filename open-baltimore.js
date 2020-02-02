2f7fd6df4578af05bb7bb17fb772a3be3e23ad22
Shane Knudsen <shaneknu@gmail.com>
2020-02-02 17:06:08 -0500

    Abstracted out https function call to common function.

--- open-baltimore.js
+++ open-baltimore.js
@@ -2,30 +2,29 @@ const https = require('https');
 
 module.exports = {
     getViolationData,
-    getDataByYear
+    getDataByYear,
+    getMonthlyRecords
 }
 
 async function getViolationData(state, tag) {
-    return new Promise((resolve, reject) => {
-        https.get(
-            `https://data.baltimorecity.gov/resource/2ddy-2uzt.json?$select=violcode,count(violcode) as count&$where=tag='${tag}' AND state='${state}'&$group=violcode`,
-            (response) => {
-                let data = '';
-                response.on('data', chunk => { data += chunk });
-                response.on('end', () => {
-                    resolve(JSON.parse(data));
-                });
-            }
-        ).on('error', err => { 
-            reject(Error(err));
-        });
-    });
+    const url = `https://data.baltimorecity.gov/resource/2ddy-2uzt.json?$select=violcode,count(violcode) as count&$where=tag='${tag}' AND state='${state}'&$group=violcode`;
+    return getOpenBaltimoreData(url);
 }
 
 async function getDataByYear(state, tag) {
+    const url = `https://data.baltimorecity.gov/resource/2ddy-2uzt.json?$select=date_extract_y(violDate) as year,count(*) as count,sum(violFine) as annualFines&$where=tag='${tag}' AND state='${state}'&$group=year&$order=year`;
+    return getOpenBaltimoreData(url);
+}
+
+async function getMonthlyRecords(year, month) {
+    const url = `https://data.baltimorecity.gov/resource/2ddy-2uzt.json?$select=state,tag,make,violFine,violCode,violDate,date_extract_y(violDate) as year, date_extract_m(violDate) as month&$where=year='${year}' AND month='${month}'&$order=violDate&$limit=100000`;
+    return getOpenBaltimoreData(url);
+}
+
+async function getOpenBaltimoreData(url) {
     return new Promise((resolve, reject) => {
         https.get(
-            `https://data.baltimorecity.gov/resource/2ddy-2uzt.json?$select=date_extract_y(violDate) as year,count(*) as count,sum(violFine) as annualFines&$where=tag='${tag}' AND state='${state}'&$group=year&$order=year`,
+            url,
             (response) => {
                 let data = '';
                 response.on('data', chunk => { data += chunk });
